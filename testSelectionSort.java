108a31567e9a27d1577289ec56d27af2aef6ece6
Nabeel <nsahmed@umich.edu>
2020-12-03 17:15:30 -0500

    implemented testNegative and testPositive, fixed SelectionSort logic

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -32,32 +32,31 @@ public class testSelectionSort {
         Sortedarr[4] = 10;
 
 
-
-
-
-
+        SelectionSort s = new SelectionSort();
+        s.basicSelectionSort(arr);
+        assertArrayEquals(Sortedarr, arr);
     }
 
     @Test
     public void testNegative(){
-        /** Test data contains negative values only **/
+      /** Test data contains negative values only **/
+      int[] arr = new int[5];
+      arr[0] = -8;
+      arr[1] = -9;
+      arr[2] = -7;
+      arr[3] = -10;
+      arr[4] = -2;
 
+      int[] Sortedarr = new int[5];
+       Sortedarr[0] = -10;
+      Sortedarr[1] = -9;
+      Sortedarr[2] = -8;
+      Sortedarr[3] = -7;
+      Sortedarr[4] = -2;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      SelectionSort s = new SelectionSort();
+      s.basicSelectionSort(arr);
+      assertArrayEquals(Sortedarr, arr);
     }
 
     @Test
